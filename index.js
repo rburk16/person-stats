8a4bfa71e33d8a1c3be35eed929469513676c536
Ryan Burkholder <rcburkholder@anderson.edu>
2017-06-26 17:29:56 -0400

    Change the div when the form is submitted

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ function changeHeading(ev){
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
-    const heading = document.querySelector('h1')
+    const heading = document.querySelector('#stats')
     heading.textContent = name
 }
+
 
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeHeading)
