14583399a9aa895c8c1eeff145f2ebe8d93f7646
Ryan Burkholder <rcburkholder@anderson.edu>
2017-06-27 09:55:22 -0400

    Change the paragraph font color to match the value of the second input

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+const myArray = []
+const arrayList = []
+
 function changeHeading(ev){
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
-    const heading = document.querySelector('#hometown')
-    heading.textContent = name
+    const heading = document.querySelector('h1')
+    heading.innerHTML = name
+    myArray.push(name)
+}
+
+function changeParagraph(ev){
+    ev.preventDefault()
+    const f = ev.target
+    const color = f.personColor.value
+    const paragraph = document.querySelector('#someString')
+    arrayList.push(color)
+    paragraph.textContent = myArray[0] + arrayList
+    paragraph.style.color = color
 }
 
 
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeHeading)
+
+const colorForm = document.querySelector('#person-form')
+colorForm.addEventListener('submit', changeParagraph)
